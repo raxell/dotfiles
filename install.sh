b6868ab52cfd6f39814000dc5661c654581719f3
Marco Comes <m.comes@gmail.com>
2018-03-15 19:32:33 +0100

    Remove vim if already installed and add dependencies

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 cd ~
 
+# Remove Vim if already installed
+sudo apt remove vim vim-runtime gvim
+
+# Install Vim dependencies
+sudo apt install libncurses5-dev libgnome2-dev libgnomeui-dev \
+    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev
+
 # Install Vim
 if [ ! -d ~/vim ]; then
 	git clone https://github.com/vim/vim.git
 	cd vim
 	./configure --with-features=huge
 	make
-	make install
+	sudo make install
 fi
 
 # Setup Vim
