2bcaef2f87773c05b76aa70d076ff47414fd871d
Marco Comes <comes95@gmail.com>
2018-03-30 09:44:51 +0200

    Use same indentation style

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ sudo apt remove vim vim-runtime gvim
 
 # Install Vim dependencies
 sudo apt install libncurses5-dev libgnome2-dev libgnomeui-dev \
-    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev
+	libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+	libcairo2-dev libx11-dev libxpm-dev libxt-dev
 
 # Install editorconfig-vim dependencies
 sudo apt-get install editorconfig
