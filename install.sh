8908dfba0f0378f7a63e44d8bb651c0bc3908f06
Marco Comes <comes95@gmail.com>
2018-12-12 18:41:31 +0100

    Add terminal theme

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 cd ~
 
+# Load terminal theme file
+dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < ~/dotfiles/terminal-theme.dconf
+
 sudo apt update
 
 sudo apt install build-essential
