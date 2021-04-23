52f041cb3f1c4b9a9e08250bccf5b12fa496cdec
Marco Comes <comes95@gmail.com>
2021-04-24 00:31:57 +0200

    Don't hard code the dotfiles directory

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+DOTFILES_DIR=$(cd `dirname $0` && pwd)
+
 if [ -z "$ME" ]; then
     echo "Missing environment variable ME"
     exit 1
@@ -11,26 +13,26 @@ if [ $USER != $ME ]; then
 fi
 
 # Setup Git
-ln -sf "$HOME/dotfiles/gitconfig" "$HOME/.gitconfig"
-ln -sf "$HOME/dotfiles/gitignore" "$HOME/.gitignore"
+ln -sf "$DOTFILES_DIR/gitconfig" "$HOME/.gitconfig"
+ln -sf "$DOTFILES_DIR/gitignore" "$HOME/.gitignore"
 
 # Setup bash
-ln -sf "$HOME/dotfiles/bashrc" "$HOME/.bashrc"
+ln -sf "$DOTFILES_DIR/bashrc" "$HOME/.bashrc"
 
 # Add man page for z script
 sudo mkdir -p /usr/local/man/man1
-sudo ln -sf "$HOME/dotfiles/z/z.1" /usr/local/man/man1/z.1
+sudo ln -sf "$DOTFILES_DIR/z/z.1" /usr/local/man/man1/z.1
 sudo mandb
 
 # Setup todo-txt
 mkdir "$HOME/.todo"
-ln -sf "$HOME/dotfiles/todo/config" "$HOME/.todo/config"
-ln -sf "$HOME/dotfiles/todo/actions" "$HOME/.todo/actions"
+ln -sf "$DOTFILES_DIR/todo/config" "$HOME/.todo/config"
+ln -sf "$DOTFILES_DIR/todo/actions" "$HOME/.todo/actions"
 
 # Tmux setup
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -sf "$HOME/dotfiles/tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$DOTFILES_DIR/tmux.conf" "$HOME/.tmux.conf"
 
 # Zsh setup
-ln -sf "$HOME/dotfiles/zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES_DIR/zshrc" "$HOME/.zshrc"
 
